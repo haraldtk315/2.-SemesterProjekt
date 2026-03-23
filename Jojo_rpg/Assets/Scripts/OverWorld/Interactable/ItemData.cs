77ff44a29ffe41f5ba93e152e7dcf12b98ca3cf0
EmmaTheCattt <Emmabuhl2004@gmail.com>
2026-03-23 13:37:23 +0100

    item update

--- Jojo_rpg/Assets/Scripts/OverWorld/Interactable/ItemData.cs
+++ Jojo_rpg/Assets/Scripts/OverWorld/Interactable/ItemData.cs
@@ -6,6 +6,15 @@ public class ItemData : ScriptableObject
     public string itemID;
     public string displayName;
     public Sprite icon;
+    public ItemType Type;
+    public int value;
+
+    public enum ItemType
+    {
+        health,
+        focus,
+        revive
+    }
 
     [TextArea]
     public string description;
