04e0828ae2415476f58eb058e4aa53844db41bb8
EmmaTheCattt <Emmabuhl2004@gmail.com>
2026-05-22 15:15:54 +0200

    update

--- Jojo_rpg/Assets/Scripts/STORYTELLER.cs
+++ Jojo_rpg/Assets/Scripts/STORYTELLER.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class STORYTELLER : MonoBehaviour
 {
+
+    public string[] Narrator1;
+    public int ON_THIS;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        DIALOGUEHANDLER.instance.DialogueStart(Narrator1, this.gameObject, null);
     }
 
     // Update is called once per frame
